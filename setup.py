f87738d24407e9b593dab9a909e6520a7c4bcfaf
Raghav Sethi <work.raghavsethi@gmail.com>
2025-02-07 12:38:35 +0530

    fix revision 0.0.71

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 # Define package metadata
 NAME = "flask-oauth-guard"
-VERSION = "0.0.6"
+VERSION = "0.0.71"
 AUTHOR = "Raghav Sethi"
 AUTHOR_EMAIL = "work.raghavsethi@gmail.com"
 DESCRIPTION = "A Flask extension to add Authentication and Authorization, supports OAuth2"
